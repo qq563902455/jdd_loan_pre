19794f412758a67de2d75f24e6dc355a677ada20
qq563902455 <563902455@qq.com>
2017-12-10 11:24:12 +0800

    根据相关系数 人为挑选特征，最后达到 cv 1.79468  lb(B)  1.778378

---
+++ artificialFeatureGernerator.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 10:41:27 2017
+
+@author: 56390
+"""
+
+
+import pandas as pd;
+import numpy  as np;
+import copy as cp;
+import math;
+from sklearn.metrics import mean_squared_error;
+from sklearn.model_selection import KFold; 
+
+
+class artificialFeatureGernerator:
+    def __init__(self,model,dataX,dataY,kFold=KFold(n_splits=5,random_state=0),verbose=False):
+        self.model=model;
+        self.dataX=dataX;
+        self.dataY=dataY;
+        self.kFold=kFold;
+        self.verbose=verbose;
+    def fit(self):
+        artificialFeatures=self.dataY.copy();
+        for kTrainIndex,kTestIndex in self.kFold.split(self.dataX,self.dataY):
+            kTrain_x=self.dataX.iloc[kTrainIndex];  
+            kTrain_y=self.dataY.iloc[kTrainIndex]; 
+            
+            kTest_x=self.dataX.iloc[kTestIndex];  
+            kTest_y=self.dataY.iloc[kTestIndex]; 
+            
+            for col in kTrain_y.columns:
+                self.model.fit(kTrain_x,kTrain_y[col]);
+                colPre=self.model.predict(kTest_x);
+                self.verbosePrint(self,col+' rmse: ',math.sqrt(mean_squared_error(kTest_y, colPre)))
+                artificialFeatures.iloc[kTestIndex,col]=colPre;            
+        return artificialFeatures;
+            
+        
+    def verbosePrint(self,out):
+        if self.verbose==True:
+            print(out);
--- task.py
+++ task.py
@@ -35,6 +35,108 @@ def getEncryptVal(val):
 t_vis=pd.read_csv("D:/general_file_unclassified/about_code/JDD/Loan_pre/data/t_vis.csv");
 t_vis=t_vis.drop(['Unnamed: 0'],axis=1);
 
+t_label_list=['t_loan_loan_amount_8','t_loan_loan_amount_9','t_loan_loan_amount_10','t_loan_loan_amount_11'];
+
+'''
+t_loan部分的一些处理
+'''
+t_loan_list=[];
+for col in t_vis.columns:
+    if 't_loan' in col:
+        t_loan_list.append(col);
+t_loan_corr_table=t_vis[t_loan_list].corr();
+
+corr_rank=pd.Series();
+temp=t_loan_corr_table[t_label_list].abs().sum(axis=1);
+for col in t_loan_corr_table.columns:
+    if '_8' in col:
+        corr_rank[col[0:(len(col)-2)]]=temp[col[0:(len(col)-2)]+'_8']+temp[col[0:(len(col)-2)]+'_9']+temp[col[0:(len(col)-2)]+'_10']+temp[col[0:(len(col)-2)]+'_11'];
+
+t_loan_remain=corr_rank.sort_values(ascending=False).head(20).index.tolist();
+print('t_loan_remian: ',len(t_loan_remain));
+t_loan_remainlist=[];
+for col in t_loan_remain:
+    t_loan_remainlist.append(col+'_8');
+    t_loan_remainlist.append(col+'_9');
+    t_loan_remainlist.append(col+'_10');
+    t_loan_remainlist.append(col+'_11');
+
+dropList=[];    
+for col in  t_loan_list:
+    if col not in t_loan_remainlist:
+        dropList.append(col);
+t_vis=t_vis.drop(dropList,axis=1);
+
+
+'''
+t_click部分的处理
+'''
+
+
+t_click_list=[];
+for col in t_vis.columns:
+    if 't_click' in col:
+        t_click_list.append(col);
+corr_table=t_vis[t_click_list+t_label_list].corr();
+
+corr_rank=pd.Series();
+temp=corr_table[t_label_list].abs().sum(axis=1);
+for col in corr_table.columns:
+    if '_8' in col:
+        corr_rank[col[0:(len(col)-2)]]=temp[col[0:(len(col)-2)]+'_8']+temp[col[0:(len(col)-2)]+'_9']+temp[col[0:(len(col)-2)]+'_10']+temp[col[0:(len(col)-2)]+'_11'];
+
+t_click_remain=corr_rank.sort_values(ascending=False).head(30).index.tolist();
+print('t_click_remian: ',len(t_click_remain));
+
+t_click_remainlist=[];
+for col in t_click_remain:
+    t_click_remainlist.append(col+'_8');
+    t_click_remainlist.append(col+'_9');
+    t_click_remainlist.append(col+'_10');
+    t_click_remainlist.append(col+'_11');
+
+dropList=[];    
+for col in  t_click_list:
+    if col not in t_click_remainlist:
+        dropList.append(col);
+t_vis=t_vis.drop(dropList,axis=1);
+
+
+
+'''
+t_order部分的处理
+'''    
+
+t_order_list=[];
+for col in t_vis.columns:
+    if 't_order' in col:
+        t_order_list.append(col);
+corr_table=t_vis[t_order_list+t_label_list].corr();
+
+corr_rank=pd.Series();
+temp=corr_table[t_label_list].abs().sum(axis=1);
+for col in corr_table.columns:
+    if '_8' in col:
+        corr_rank[col[0:(len(col)-2)]]=temp[col[0:(len(col)-2)]+'_8']+temp[col[0:(len(col)-2)]+'_9']+temp[col[0:(len(col)-2)]+'_10']+temp[col[0:(len(col)-2)]+'_11'];
+
+t_order_remain=corr_rank.sort_values(ascending=False).head(1).index.tolist();
+print('t_click_remian: ',len(t_order_remain));
+
+t_order_remainlist=[];
+for col in t_order_remain:
+    t_order_remainlist.append(col+'_8');
+    t_order_remainlist.append(col+'_9');
+    t_order_remainlist.append(col+'_10');
+    t_order_remainlist.append(col+'_11');
+
+dropList=[];    
+for col in  t_order_list:
+    if col not in t_order_remainlist:
+        dropList.append(col);
+t_vis=t_vis.drop(dropList,axis=1);
+
+
+
 '''
 拆分训练集，以及测试集
 '''
@@ -43,7 +145,7 @@ for col in t_vis.columns:
     if '_11' in col:
         colTrainDrop.append(col);
 dataTrainX=t_vis.drop(colTrainDrop,axis=1);
-dataTrainY=t_vis['loan_amount_11'].apply(getEncryptVal);
+dataTrainY=t_vis[t_label_list[3]].apply(getEncryptVal);
 
 colPreDrop=['uid'];
 for col in t_vis.columns:
@@ -67,22 +169,28 @@ for col in dataTrainX.columns:
 dataTrainX.columns=colNameList;
 dataPre.columns=colNameList;
 
-colCatList=['sex','cate_id_0','cate_id_1','cate_id_2',
-                  'param_0','param_1','param_2',
-                  'pid_0','pid_1','pid_2',];
-            
-for col in dataTrainX.drop(colCatList,axis=1).columns:
-    if '_0' in col:
-        dataTrainX[col[0:len(col)-2]+'_sum012']=dataTrainX[col[0:len(col)-2]+'_0']+dataTrainX[col[0:len(col)-2]+'_1']+dataTrainX[col[0:len(col)-2]+'_2'];
-        dataPre[col[0:len(col)-2]+'_sum012']=dataPre[col[0:len(col)-2]+'_0']+dataPre[col[0:len(col)-2]+'_1']+dataPre[col[0:len(col)-2]+'_2'];
-            
-colScList=dataTrainX.drop(colCatList,axis=1).columns.values.tolist();       
-            
-preprocessor=rawDataProcess(dataTrainX,dataPre,colCatList,colScList);
+possibleColCatList=['sex','t_loan_mostly_loan_day_or_night_0','t_loan_mostly_loan_day_or_night_1','t_loan_mostly_loan_day_or_night_2',
+                 't_loan_mostly_loan_hour_0','t_loan_mostly_loan_hour_1','t_loan_mostly_loan_hour_2'];
 
-dataTrainX=preprocessor.toTrainData();
-dataPre=preprocessor.toTestData();
-print(dataPre.head())
+colCatList=[];
+for col in possibleColCatList:
+    if col in dataTrainX.columns:
+        colCatList.append(col);
+
+#colCatList=[];
+            
+#for col in dataTrainX.drop(colCatList,axis=1).columns:
+#    if '_0' in col:
+#        dataTrainX[col[0:len(col)-2]+'_sum012']=dataTrainX[col[0:len(col)-2]+'_0']+dataTrainX[col[0:len(col)-2]+'_1']+dataTrainX[col[0:len(col)-2]+'_2'];
+#        dataPre[col[0:len(col)-2]+'_sum012']=dataPre[col[0:len(col)-2]+'_0']+dataPre[col[0:len(col)-2]+'_1']+dataPre[col[0:len(col)-2]+'_2'];
+            
+#colScList=dataTrainX.drop(colCatList,axis=1).columns.values.tolist();       
+#            
+#preprocessor=rawDataProcess(dataTrainX,dataPre,colCatList,colScList);
+#
+#dataTrainX=preprocessor.toTrainData();
+#dataPre=preprocessor.toTestData();
+#print(dataPre.shape);
 
 
 '''
@@ -156,7 +264,7 @@ def selfEvalMetric(y_true, y_pred):
     return ('rmse',rmseScoreCal(y_true, y_pred),False);
 
 
-Reg=lgb.LGBMRegressor(subsample=0.9,colsample_bytree=0.8,subsample_for_bin=220000,min_child_samples=400,subsample_freq=2,max_depth=8,random_state=666,n_estimators=350,learning_rate=0.02,verbose=1);
+Reg=lgb.LGBMRegressor(subsample=0.9,colsample_bytree=0.8,subsample_for_bin=220000,min_child_samples=400,subsample_freq=2,max_depth=8,random_state=666,n_estimators=255,learning_rate=0.02,verbose=1);
 kfold=KFold(n_splits=5,random_state=1);
 rmseScoreCalList=[];
 for kTrainIndex,kTestIndex in kfold.split(dataTrainX,dataTrainY):
@@ -167,7 +275,8 @@ for kTrainIndex,kTestIndex in kfold.split(dataTrainX,dataTrainY):
         kTest_y=dataTrainY.iloc[kTestIndex]; 
         
         
-#        Reg.fit(kTrain_x,kTrain_y,eval_set=(kTest_x,kTest_y),eval_metric=selfEvalMetric,verbose=True,early_stopping_rounds=50);
+#        Reg.fit(kTrain_x,kTrain_y,eval_set=(kTest_x,kTest_y),eval_metric=selfEvalMetric,verbose=False,early_stopping_rounds=50);
+#        print('best iteration: ',Reg.best_iteration_);
         Reg.fit(kTrain_x,kTrain_y);
         testPre=Reg.predict(kTest_x);
         
@@ -180,7 +289,7 @@ print('mean rmse:',np.array(rmseScoreCalList).mean());
 '''
 删除部分特征以达到更好的效果
 '''
-estimator = lgb.LGBMRegressor(subsample=0.9,colsample_bytree=0.8,subsample_for_bin=220000,min_child_samples=400,subsample_freq=2,max_depth=8,random_state=666,n_estimators=350,learning_rate=0.02,verbose=1);
+estimator = lgb.LGBMRegressor(subsample=0.9,colsample_bytree=0.8,subsample_for_bin=220000,min_child_samples=400,subsample_freq=2,max_depth=8,random_state=666,n_estimators=255,learning_rate=0.02,verbose=1);
 selector = RFECV(estimator, step=20, cv=KFold(n_splits=5,random_state=1),scoring=rmseScorer,verbose=True);
 selector = selector.fit(dataTrainX,dataTrainY);
 print(len(selector.grid_scores_));
